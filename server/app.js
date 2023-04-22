a683d877b0e59609daa328513c56191a3bef92ba
Ricky <jacobus.kim@gmail.com>
2023-04-22 18:53:40 +0200

    repush

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-require("dotenv").config();
+// require("dotenv").config();
 
 
 const PORT = process.env.PORT || 5000
